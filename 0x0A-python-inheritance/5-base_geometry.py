bc26f81c17980efff7bfd210c308c563bda52a3a
Osvic1 <timothyv952@gmail.com>
2023-02-06 06:44:14 -0800

    an empty class BaseGeometry

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines an empty class BaseGeometry."""
+
+
+class BaseGeometry:
+    """
+    This class represents a basic geometry object with no implementation.
+    """
+    pass
