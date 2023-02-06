eaa50d34fced07f7a9f954a9cf4160868f5e5ccc
Osvic1 <timothyv952@gmail.com>
2023-02-06 07:44:50 -0800

    hot fix

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -27,5 +27,6 @@ class BaseGeometry:
         """
         if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
+
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
