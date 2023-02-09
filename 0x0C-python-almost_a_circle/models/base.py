69b6aeeb9d547426d6b4677e88a6c5b9e36ed8f2
Osvic1 <timothyv952@gmail.com>
2023-02-09 07:16:39 -0800

    test models

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''Module for Base class.'''
+from json import dumps, loads
+import csv
+
+
+class Base:
+ '''A representation of the base of our OOP hierarchy.'''
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        '''Constructor'''
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
