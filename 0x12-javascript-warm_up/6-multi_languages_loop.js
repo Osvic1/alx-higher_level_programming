b57dec29fed1d1b6e4dba57e2cd57fbb92c20014
Osvic1 <timothyv952@gmail.com>
2023-04-11 11:59:27 -0700

    6-multi_languages_loop.js updated

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,3 @@
 #!/usr/bin/node
-for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
-  console.log(line);
-}
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const line of lines) console.log(line);
