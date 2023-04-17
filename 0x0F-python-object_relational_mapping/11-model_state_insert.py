47d6039a88a5d5c634c89027a9d6f10c857d14d4
Osvic1 <timothyv952@gmail.com>
2023-04-17 03:12:49 -0700

    11-model_state_insert.py

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,20 +1,35 @@
 #!/usr/bin/python3
-""" prints the State object with the name passed as argument from the database
-"""
-import sys
-from model_state import Base, State
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
 
+"""
+    This script inserts the State object louisiana
+    to the database hbtn_0e_6_usa
+"""
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    Base.metadata.create_all(engine)
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+
+    driver = "mysql+mysqldb"
+    uname = sys.argv[1]
+    password = sys.argv[2]
+    dbname = sys.argv[3]
+
+    engine = create_engine(
+            '{}://{}:{}@localhost:3306/{}'.format(
+                driver,
+                uname,
+                password,
+                dbname),
+            pool_pre_ping=True)
+
     Session = sessionmaker(bind=engine)
     session = Session()
-    new_state = State(name='Louisiana')
+
+    new_state = State(name="Louisiana")
+
     session.add(new_state)
-    new_instance = session.query(State).filter_by(name='Louisiana').first()
-    print(new_instance.id)
     session.commit()
+
+    print(new_state.id)
