79e3cb41835369e5d4663588be8f38ab00e2bfc4
Osvic1 <timothyv952@gmail.com>
2023-04-17 03:17:12 -0700

    fixed

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,18 +1,34 @@
 #!/usr/bin/python3
-""" prints the State object with the name passed as argument from the database
-"""
-import sys
-from model_state import Base, State
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
 
+"""
+    This script edits the State object from
+    the database hbtn_0e_6_usa with id = 2
+"""
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+
+    driver = "mysql+mysqldb"
+    uname = sys.argv[1]
+    password = sys.argv[2]
+    dbname = sys.argv[3]
+
+    engine = create_engine(
+            '{}://{}:{}@localhost:3306/{}'.format(
+                driver,
+                uname,
+                password,
+                dbname),
+            pool_pre_ping=True)
+
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    new_instance = session.query(State).filter_by(id=2).first()
-    new_instance.name = 'New Mexico'
+
+    session.query(State).filter(State.id == 2).update(
+            {State.name: "New Mexico"})
+
     session.commit()
